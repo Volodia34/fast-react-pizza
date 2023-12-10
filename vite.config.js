ea6a68ff104d90ed57001e0f26aae5a9028e0f92
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2023-12-10 23:31:03 +0200

    new-commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),eslint()],
+  base: '/',
+  plugins: [react(),eslint()]
 })
